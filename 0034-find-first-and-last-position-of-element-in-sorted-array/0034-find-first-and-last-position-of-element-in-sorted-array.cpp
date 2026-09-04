3f606c423e80e649f35c5f053f825854d77fca10
Shubham Chowdhury <laptop.sc.me@gmail.com>
2026-09-04 23:17:12 +0530

    Time: 0 ms (100%), Space: 17.6 MB (18.3%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -14,11 +14,11 @@ public:
                 first = mid;
                 end = mid - 1;
             }
-            else if(arr[mid] > target){
-                end = mid - 1;
+            else if(arr[mid] < target){
+                start = mid + 1;
             }
             else {
-                start = mid + 1;
+                end = mid - 1;
             }
         }
 
